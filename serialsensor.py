d9b7d9be1a1d2ee992bcb44cd7caea0acc19ca8d
McBride-SallyJoeBob <39777219+McBride-SallyJoeBob@users.noreply.github.com>
2022-03-08 15:59:39 -0500

    increase functionality to serialsensor.py create baseline for SQL server pipeline

--- serialsensor.py
+++ serialsensor.py
@@ -11,13 +11,34 @@ for i in range (0,500):
     output = ""
     try:
         output = raw.decode()
-        output = output.strip()
-
-    except UnicodeDecodeError:
+        output = output.strip()   # removing extra stuff from serial stream
+        if output[0:3] == "///":
+            output = ""            # nothing is printed
+        if output[0:3] == "###":
+            output = "INFO: " + output[3:]   # info
+            print(output)
+        elif output[0:3] == "!!!":
+            output = "ERROR: " + output[3:]    # error
+            print(output)
+        elif output[0:3] == "$$$":
+            data = output[3:]
+            data = data.split(' ')  # MUST BE OF LENGTH 7
+            if len(data) != 7:
+                raise Exception("Invalid sensor data format")
+            sense_id = int(data[0])
+            name = data[1]
+            num_outputs = int(data[2])
+            series_names = data[3].split(',')
+            outputs = [float(var) for var in data[4].split(',')]
+            units = data[5].split(',')
+            time = float(data[6])
+            # TODO Send packet with this datapoint to the SQL server
+        else:
+            print(output)  # extra serial stuff
+    except UnicodeDecodeError:  # serial decode didnt work
         output = raw
-    log.write(output + "\n")
-    print(output)
-
+    if output != "":
+        log.write(output + "\n")
 serial_in.close()
 log.close()
 exit()
