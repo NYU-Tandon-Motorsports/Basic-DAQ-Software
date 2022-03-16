4adf9aebcb930697c231b676963a469e6d16a0cc
McBride-SallyJoeBob <39777219+McBride-SallyJoeBob@users.noreply.github.com>
2022-03-16 19:52:16 -0400

    got hall effect working

--- potentiometer/potentiometer.ino
+++ potentiometer/potentiometer.ino
@@ -1,15 +1,19 @@
 void setup() 
 {
+    pinMode(A0,INPUT);
+    pinMode(A2,INPUT);
+    analogReference(EXTERNAL);
     Serial.begin(9600);
-    Serial.println("###Potentiometer Test");
+    Serial.println("###Potentiometer + Hall effect Test");
 }
 
 void loop() {
-    
-    int voltage = analogRead(A0);
-    Serial.print("$$$1 SteeringAngle 1 voltage ");
-    Serial.print(voltage);
-    Serial.print(" V ");
-    Serial.println(millis()/1000.0);
+    double voltage = analogRead(A0);
+    //Serial.print("$$$1 SteeringAngle 1 voltage ");
+    //Serial.print(voltage);
+    //Serial.print(" V ");
+    //Serial.println(millis()/1000.0);
+    Serial.println(analogRead(A2));
+    Serial.println(digitalRead(A2));
     delay(100);
 }
