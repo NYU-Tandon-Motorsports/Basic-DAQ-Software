e5aa0a9cb74e1c5ac1076ac80e9a50cc91ed22ef
McBride-SallyJoeBob <39777219+McBride-SallyJoeBob@users.noreply.github.com>
2022-05-25 18:45:46 -0400

    improved gps test

--- testgps/testgps.py
+++ testgps/testgps.py
@@ -1,3 +1,12 @@
-import GPS
-GPS.testGPS()
+import serial
+import time
+print("INFO: Connecting GPS Port..")
+try:
+    serw = serial.Serial("/dev/ttyUSB2", baudrate = 115200, timeout = 1,rtscts=True, dsrdtr=True)
+    serw.write('AT+QGPS=1\r'.encode())
+    serw.close()
+    time.sleep(1)
+except Exception as e:
+    print("ERROR: Serial port connection failed.")
+    print(e)
 exit(0)
