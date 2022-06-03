c28d33192e2c8c6dd8aaabe9b9cdf3deeea15c69
McBride-SallyJoeBob <39777219+McBride-SallyJoeBob@users.noreply.github.com>
2022-06-03 13:07:08 -0400

    create kill script

--- justrun.sh
+++ justrun.sh
@@ -1,14 +1,3 @@
 chmod +x serialsensor.py
-control_c() {
-    pkill -9 -f serialsensor.py
-    exit
-}
-
-trap control_c SIGINT
-
-while true ; do
-   python3 serialsensor.py | while read line ; do
-   echo $line
-   done
-done
+python3 serialsensor.py
 /bin/bash
