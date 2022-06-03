4672fae6504b31cb8b0558add6613a353bbf0935
McBride-SallyJoeBob <39777219+McBride-SallyJoeBob@users.noreply.github.com>
2022-06-03 13:02:35 -0400

    test kb interrupt

--- justrun.sh
+++ justrun.sh
@@ -1,3 +1,14 @@
 chmod +x serialsensor.py
-python3 serialsensor.py
+control_c() {
+    pkill -9 -f serialsensor.py
+    exit
+}
+
+trap control_c SIGINT
+
+while true ; do
+   python3 serialsensor.py | while read line ; do
+   echo $line
+   ...
+done
 /bin/bash
