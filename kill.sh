04715c95ad44b3d293c4af1cccc0d1d467e58115
McBride-SallyJoeBob <39777219+McBride-SallyJoeBob@users.noreply.github.com>
2022-06-04 09:11:46 -0400

    kill command posts notification

--- kill.sh
+++ kill.sh
@@ -1,1 +1,12 @@
 pkill -9 -f serialsensor.py
+D=$(date +%FT%T%z)
+curl --location --request POST 'https://nyu-baja-telemetry.herokuapp.com/measurement/' \
+--header 'Content-Type: application/json' \
+--header 'Cookie: Cookie_2=value' \
+--data-raw '{
+	"sensor_id": 5,
+	"values": {
+		"log" : "DAQ process sucessfully killed by Pit"
+	},
+	"date" : "'"$D"'"
+}'
