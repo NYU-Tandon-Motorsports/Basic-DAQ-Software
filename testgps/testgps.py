14a21500522ff8aaf3f5356ef764e188bee36d46
McBride-SallyJoeBob <39777219+McBride-SallyJoeBob@users.noreply.github.com>
2023-01-14 16:09:56 -0500

    fix gps with arduinos

--- testgps/testgps.py
+++ testgps/testgps.py
@@ -1,12 +1,21 @@
 import serial
 import time
+from serial.tools import list_ports
 print("INFO: Connecting GPS Port..")
+
+device_signature= '2c7c:0125'
+candidates=list(list_ports.grep(device_signature))
+candidates.reverse()
+
+print([candidate.device for candidate in candidates])
+
 try:
-    serw = serial.Serial("/dev/ttyUSB2", baudrate = 115200, timeout = 1,rtscts=True, dsrdtr=True)
+    print("writing to "+ candidates[2].device)
+    serw = serial.Serial(candidates[2].device, baudrate = 115200, timeout = 1,rtscts=True, dsrdtr=True)
     serw.write('AT+QGPS=1\r'.encode())
     serw.close()
     time.sleep(1)
 except Exception as e:
     print("ERROR: Serial port connection failed.")
     print(e)
-exit(0)
+exit(int(candidates[1].device[-1]))
--- testgps/testgps.sh
+++ testgps/testgps.sh
@@ -2,6 +2,7 @@ killall gpsmon
 killall gpsd
 chmod +x testgps.py
 python3 testgps.py
-sudo gpsd /dev/ttyUSB1 -F /var/run/gpsd.sock
-sudo gpsmon /dev/ttyUSB1
+RC=$?
+sudo gpsd /dev/ttyUSB${RC} -F /var/run/gpsd.sock
+sudo gpsmon /dev/ttyUSB${RC}
 /bin/bash
