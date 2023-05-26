6746996269a9086981ed9e6484b3c88ff7cadea4
McBride-SallyJoeBob <39777219+McBride-SallyJoeBob@users.noreply.github.com>
2023-05-25 20:41:19 -0400

    add data limiter to mercury3

--- kill.sh
+++ kill.sh
@@ -1,10 +1,10 @@
 pkill -9 -f serialsensor.py
 D=$(date +%FT%T%z)
-curl --location --request POST 'https://nyu-baja-telemetry.herokuapp.com/measurement/' \
+curl --location --request POST 'https://telemetry-nyubaja.herokuapp.com/measurement/' \
 --header 'Content-Type: application/json' \
 --header 'Cookie: Cookie_2=value' \
 --data-raw '{
-	"sensor_id": 5,
+	"sensor_id": 2,
 	"values": {
 		"log" : "DAQ process sucessfully killed by Pit"
 	},
