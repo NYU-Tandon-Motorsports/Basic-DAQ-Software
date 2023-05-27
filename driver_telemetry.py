e0d61d266eb253e15302024429a8cc62cbc4927a
McBride-SallyJoeBob <39777219+McBride-SallyJoeBob@users.noreply.github.com>
2023-05-26 20:59:32 -0400

    test buttone11

--- driver_telemetry.py
+++ driver_telemetry.py
@@ -45,11 +45,12 @@ display_right = tm1637.TM1637(clk=16, dio=21) if ENABLE_7_SEG else None
 
 executor = ThreadPoolExecutor(max_workers=1)
 
-# car states
-accel_state = 0
-angle_state = 0
+# car states -> change based on  how you chose to read your data
+speed_state = 0
+rpm_state = 0
 current_time = 0
-
+best_lap = "00:00:00"
+past_lap = "00:00:00"
 
 
 
@@ -88,7 +89,7 @@ def pygame_task():
         #print("hello1")
 
         # SPEEDOMETER
-        write_text(screen, "Speedometer", 30, (WIDTH / 4, (HEIGHT / 2) - (clock_radius / 2) - 60))
+        write_text(screen, "Speedometer", 30, (WIDTH / 4, (HEIGHT / 2) - (clock_radius / 2) - 35))
         #print("hello1.1")
 
         # clock outline
@@ -96,14 +97,14 @@ def pygame_task():
         # clock numbers
         #print("hello1.2")
 
-        clock_nums(screen, 0, 8, 5, 40, (clock_radius - 65), 38.57143, 223.2, (WIDTH / 4), (HEIGHT / 2) + 45)
+        clock_nums(screen, 0, 40, 5, 40, (clock_radius - 65), 38.57143, 223.2, (WIDTH / 4), (HEIGHT / 2) + 65)
         # ticks
-        ticks(screen, 0, 36, (clock_radius - 15), 7.714286, 223.2, WIDTH / 4, (HEIGHT / 2) + 45)
+        ticks(screen, 0, 36, (clock_radius - 15), 7.714286, 223.2, WIDTH / 4, (HEIGHT / 2) + 65)
         # speed = myfont.render(str(int(MPH_state )), 1, (255, 255, 255))
         #
         #
         #
-        speed = angle_state
+        speed = speed_state
         #print(speed)
         #speed=5
         #
@@ -113,35 +114,60 @@ def pygame_task():
             speed = 0
         if (speed > 35):
             speed = 35
-        theta = (speed * (270 / 35.0)) + 223.2
+        theta = (speed * (270.0 / 35.0)) + (223.2 - (270.0 / 35.0))
+        # draw line on gauge indicating current speed
         pygame.draw.line(screen, PINK, ((WIDTH / 2) / 2, HEIGHT / 2 + 45),
                          polar_to_cartesian(140, theta, WIDTH / 4, (HEIGHT / 2) + 45), 4)
+        # print speed below gauge
+        str_speed = str(speed)
+        pygame.draw.rect(screen, PINK, [WIDTH / 4.8, HEIGHT - 55, WIDTH / 12, HEIGHT / 9], 3)
+        write_text(screen,str_speed, 50, (WIDTH / 4, (HEIGHT - 30)))
 
         # RPM
-        write_text(screen, "RPM Gauge", 30, ((WIDTH / 4) * 3, (HEIGHT / 2) - (clock_radius / 2) - 60))
+        write_text(screen, "RPM Gauge", 30, ((WIDTH / 4) * 3, (HEIGHT / 2) - (clock_radius / 2) - 35))
         # gauge outline
         #pygame.draw.circle(screen, WHITE, ((WIDTH / 4) * 3, (HEIGHT / 2) + 45), clock_radius - 10, 10)
+
+
         circle_to_arc = 75
         # danger arc
         pygame.draw.arc(screen, PINK, (800, (((HEIGHT - (clock_radius * 2)) / 2) + (circle_to_arc / 2) + 100),
                                         ((clock_radius * 2) - circle_to_arc), ((clock_radius * 2) - circle_to_arc)),
                         5.5, 6.58, 5)
         # clock numbers
-        clock_nums(screen, 1, 11, 1, 40, (clock_radius - 65), 30, 193.2, (WIDTH / 4) * 3, (HEIGHT / 2) + 45)
+        clock_nums(screen, 0, 5500, 500, 20, (clock_radius - 65), 27, 223.2, (WIDTH / 4) * 3, (HEIGHT / 2) + 65)
         # ticks
-        ticks(screen, 0, 91, (clock_radius - 15), 3, 223.2, (WIDTH / 4) * 3, (HEIGHT / 2) + 45)
+        ticks(screen, 0, 101, (clock_radius - 15), 2.7, 223.2, (WIDTH / 4) * 3, (HEIGHT / 2) + 65)
         # rpm = myfont.render(str(int(RPM_state)), 1, (255, 255, 255))
         rpm = 1
-        theta = (rpm * (270 / 9)) + 193.2
+        if rpm < 0:
+            rpm = 0
+        if rpm > 5000:
+            rpm = 5000
+        theta = (rpm * (270.0 / 50.0)) + (223.2 - (270.0 / 50.0))
         pygame.draw.line(screen, PINK, (((WIDTH / 4) * 3), (HEIGHT / 2) + 45),
                          polar_to_cartesian(140, theta, (WIDTH / 4) * 3, (HEIGHT / 2) + 45), 4)
 
+        # print RPM below gauge
+        str_rpm = str(rpm)
+        pygame.draw.rect(screen, PINK, [(WIDTH / 2) + (WIDTH / 4.8), HEIGHT - 55, WIDTH / 12, HEIGHT / 9], 3)
+        write_text(screen, str_rpm, 50, ((WIDTH / 4) * 3, (HEIGHT - 30)))
+
         # TIMER
-        # timer outline
-        pygame.draw.rect(screen, PINK, [(WIDTH / 2) - (WIDTH / 7), HEIGHT / 40 - 10, WIDTH / (3.5), HEIGHT / 7], 5)
+        # best lap
+        write_text(screen, "Best Lap", 25, (((WIDTH / 3.5) / 2) + 20, 12))
+        pygame.draw.rect(screen, PINK, [0 + 20, HEIGHT / 40 + 15, WIDTH / 3.5, HEIGHT / 8], 3)
+        write_text(screen, best_lap, 50, (((WIDTH / 3.5) / 2) + 20, HEIGHT / 8.5))
+        # current lap
+        write_text(screen, "Current Lap", 25, (((WIDTH / 3.5) / 2) + (WIDTH / 3 + 20), 12))
+        pygame.draw.rect(screen, PINK, [WIDTH / 3 + 20, HEIGHT / 40 + 15, WIDTH / 3.5, HEIGHT / 8], 3)
         start = pygame.time.get_ticks() - current_time
-        render_time(screen, start, 50)
-        #print("hello2")
+        render_time(screen, start, 50, (WIDTH / 2, HEIGHT / 8.5))
+        # previous lap
+        write_text(screen, "Past Lap", 25, (((WIDTH / 3.5) / 2) + ((WIDTH / 3) * 2 + 20), 12))
+        pygame.draw.rect(screen, PINK, [((WIDTH / 3) * 2) + 20, HEIGHT / 40 + 15, WIDTH / 3.5, HEIGHT / 8], 3)
+        write_text(screen, past_lap, 50, (((WIDTH / 3.5) / 2) + ((WIDTH / 3) * 2 + 20), HEIGHT / 8.5))
+
         pygame.display.flip()
         clock.tick(60)
 
@@ -155,13 +181,11 @@ def write_text(screen, text, size, position):
     screen.blit(text, text_rect)
 
 
-def render_time(screen, start, size):
+def render_time(screen, start, size, position):
     hundredth_of_a_second = int(str(start)[-2:])  # hundredth of a second
     time_in_ms = time((start // 1000) // 3600, ((start // 1000) // 60 % 60), (start // 1000) % 60)
     time_string = "{}{}{:02d}".format(time_in_ms.strftime("%M:%S"), ':', hundredth_of_a_second)
-    write_text(screen, time_string, size, (WIDTH / 2, HEIGHT / 13))
-
-
+    write_text(screen, time_string, size, position)
 
 # theta is in degrees
 def polar_to_cartesian(r, theta, width_center, height_center):
